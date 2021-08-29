2c2cb4c5b6ed27041e776607922e1294fe2584dd
Amr Abdelmagid <amrabdelmagid900@gmail.com>
2021-08-29 09:19:50 +0200

    Add recursion example - chapter 3

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,25 @@
 import 'package:grokking_algorithms_book_practice/chapter_1/binary_search.dart';
 import 'package:grokking_algorithms_book_practice/chapter_2/selection_sort.dart';
+import 'package:grokking_algorithms_book_practice/chapter_3/recursion.dart';
 
 void main(List<String> arguments) {
-  final binarySearchList = [1, 3, 5, 7, 9];
-  print(binarySearch(binarySearchList, 12));
+  // final binarySearchList = [1, 3, 5, 7, 9];
+  // print(binarySearch(binarySearchList, 12));
 
-  final selectionSortList = [3, 5, 12, 0, 7, 13, 1];
-  print(selectionSort(selectionSortList));
+  // final selectionSortList = [3, 5, 12, 0, 7, 13, 1];
+  // print(selectionSort(selectionSortList));
+
+  final recursionList = [
+    1,
+    [9, 3, 7],
+    2,
+    [
+      8,
+      [13, 27, 4, 32],
+      5
+    ],
+    6
+  ];
+  lookForKey(recursionList);
+  lookForKeyRecursive(recursionList);
 }
---
+++ lib/chapter_3/recursion.dart
@@ -0,0 +1,52 @@
+// We have two functions supposed to do the same functionality by applying two
+// different approaches (but actually they save difference -- illustrated
+// in the comments below)
+// lookForKey function uses loops to find a key in nested boxes
+// lookForKeyRecursive function uses recursion to do the same thing
+// the key is in box number 4!
+
+// In this function by using loops you need to refactor it if the list (the big
+// box) has more complicated nesting
+void lookForKey(List box) {
+  var pile = [];
+  box.forEach((element) {
+    if (element is int) {
+      pile.add(element);
+    }
+  });
+
+  while (pile.isNotEmpty) {
+    for (var item in box) {
+      if (item is List) {
+        item.forEach((element) {
+          if (element is List) {
+            element.forEach((element) {
+              pile.add(element);
+            });
+          } else {
+            pile.add(element);
+          }
+        });
+      } else {
+        if (pile.contains(4)) {
+          print('found the key! -- loops');
+          pile = [];
+        }
+      }
+    }
+  }
+}
+
+// But in this function by using recursion you do not need to refactor
+// it if the list (the big box) has more complicated nesting
+// This is what I meant by the two functions have differences in functionality
+
+void lookForKeyRecursive(List box) {
+  for (var item in box) {
+    if (item is List) {
+      lookForKeyRecursive(item); // Recursion
+    } else if (item == 4) {
+      print('found the key! -- recursion');
+    }
+  }
+}
