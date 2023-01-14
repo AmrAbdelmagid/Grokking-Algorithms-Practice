8cc3b7f8e63877dd534747f1d1419cb70fc35d16
Amr Abdelmagid <>
2023-01-14 22:08:45 +0200

    Update selection sort

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,9 @@
-import 'package:grokking_algorithms_book_practice/chapter_1/binary_search.dart';
-import 'package:grokking_algorithms_book_practice/chapter_2/selection_sort.dart';
-import 'package:grokking_algorithms_book_practice/chapter_3/recursion.dart';
-import 'package:grokking_algorithms_book_practice/chapter_4/divide_and_conquer.dart';
-import 'package:grokking_algorithms_book_practice/chapter_4/quicksort.dart';
-
 void main(List<String> arguments) {
   // final binarySearchList = [1, 3, 5, 7, 9];
   // print(binarySearch(binarySearchList, 12));
 
-  // final selectionSortList = [3, 5, 12, 0, 7, 13, 1];
-  // print(selectionSort(selectionSortList));
+  // final selectionSortList = [3, 5, 12, 0, 7, 13, 1, 1, -1];
+  // print(sortListBySelectionSort(selectionSortList));
 
   // final recursion = [
   //   1,
@@ -29,5 +23,5 @@ void main(List<String> arguments) {
 
   // print(listLengthRecursive([1, 2, 3, 4, 5, 6, 7]));
   // print(listMaxNumRecursive([4, 7, 7, 6, 302, 302, 32, 17, -3, 69, 7, 8, 600]));
-  print(quicksort([4, 7, 6, 302, 32, 17, -3, 69, 8, 600]));
+  // print(quicksort([4, 7, 6, 302, 32, 17, -3, 69, 8, 600]));
 }
--- lib/chapter_2/selection_sort.dart
+++ lib/chapter_2/selection_sort.dart
@@ -1,7 +1,6 @@
 /// Returns the position of the smallest number in the list
-/// as an example of selection sort algorithm
-/// O(n^2) (Big O Notation)
-int selectionSort(List list) {
+
+int _findSmallestIndex(List list) {
   var smallest = list[0];
   var smallest_index = 0;
   for (var i = 1; i < list.length; i++) {
@@ -10,6 +9,20 @@ int selectionSort(List list) {
       smallest_index = i;
     }
   }
-  print('smallest is ' + smallest.toString());
   return smallest_index;
+}
+
+/// Example of selection sort algorithm
+/// O(n^2) (Big O Notation)
+
+List sortListBySelectionSort(List list) {
+  final sortedList = [];
+  final length = list.length;
+  for (var i = 0; i < length; i++) {
+    final smallestIndex = _findSmallestIndex(list);
+    final smallestValue = list[smallestIndex];
+    sortedList.add(smallestValue);
+    list.removeAt(smallestIndex);
+  }
+  return sortedList;
 }
